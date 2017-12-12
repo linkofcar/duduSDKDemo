49cd02a534acff215406887f8ead11061eeda4e8
zhoulinxue <194093798@qq.com>
2017-12-12 17:24:53 +0800

    增加speex 压缩语音功能。

--- duduSdk/src/main/java/com/czl/chatClient/audio/MultiAudioMixer.java
+++ duduSdk/src/main/java/com/czl/chatClient/audio/MultiAudioMixer.java
@@ -24,77 +24,6 @@ public abstract class MultiAudioMixer {
         this.mOnAudioMixListener = l;
     }
 
-    /**
-     * <p>start to mix , you can call {@link #setOnAudioMixListener(OnAudioMixListener)} before this method to get mixed data.
-     */
-//    public void mixAudios(File[] rawAudioFiles){
-//
-//        final int fileSize = rawAudioFiles.length;
-//
-//        FileInputStream[] audioFileStreams = new FileInputStream[fileSize];
-//        File audioFile = null;
-//
-//        FileInputStream inputStream;
-//        byte[][] allAudioBytes = new byte[fileSize][];
-//        boolean[] streamDoneArray = new boolean[fileSize];
-//        byte[] buffer = new byte[512];
-//        int offset;
-//
-//        try {
-//
-//            for (int fileIndex = 0; fileIndex < fileSize; ++fileIndex) {
-//                audioFile = rawAudioFiles[fileIndex];
-//                audioFileStreams[fileIndex] = new FileInputStream(audioFile);
-//            }
-//
-//            while(true){
-//
-//                for(int streamIndex = 0 ; streamIndex < fileSize ; ++streamIndex){
-//
-//                    inputStream = audioFileStreams[streamIndex];
-//                    if(!streamDoneArray[streamIndex] && (offset = inputStream.read(buffer)) != -1){
-//                        allAudioBytes[streamIndex] = Arrays.copyOf(buffer,buffer.length);
-//                    }else{
-//                        streamDoneArray[streamIndex] = true;
-//                        allAudioBytes[streamIndex] = new byte[512];
-//                    }
-//                }
-//
-//                byte[] mixBytes = mixRawAudioBytes(allAudioBytes);
-//                if(mixBytes != null && mOnAudioMixListener != null){
-//                    mOnAudioMixListener.onMixing(mixBytes);
-//                }
-//
-//                boolean done = true;
-//                for(boolean streamEnd : streamDoneArray){
-//                    if(!streamEnd){
-//                        done = false;
-//                    }
-//                }
-//
-//                if(done){
-//                    if(mOnAudioMixListener != null)
-//                        mOnAudioMixListener.onMixComplete();
-//                    break;
-//                }
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            if(mOnAudioMixListener != null)
-//                mOnAudioMixListener.onMixError(1);
-//        }finally{
-//            try {
-//                for(FileInputStream in : audioFileStreams){
-//                    if(in != null)
-//                        in.close();
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
     public void mixAudios(List<byte[]> rawAudioFiles) throws  IOException{
 
         byte[] mixBytes = mixRawAudioBytes(rawAudioFiles);
@@ -103,59 +32,6 @@ public abstract class MultiAudioMixer {
         }
         if(mOnAudioMixListener != null)
             mOnAudioMixListener.onMixComplete();
-    }
-
-//    public  List<ShortData> decode(byte[] mixBytes) throws IOException{
-//        List<ShortData> list=new ArrayList<ShortData>();
-//        DataInputStream dis = new DataInputStream(
-//                new BufferedInputStream(BytesUtil.byte2Input(mixBytes)));
-//        int subSize=speex.getFrameSize();
-//        int count = mixBytes.length % subSize == 0 ? mixBytes.length / subSize: mixBytes.length / subSize + 1;
-//        for(int i=0;i<count;i++) {
-//            short[] lin = new short[subSize];
-//            byte[] encoded = new byte[subSize];
-//            ShortData data = new ShortData();
-//            dis.read(encoded,0,encoded.length);
-//            int size = speex.decode(encoded, lin,
-//                    encoded.length);
-//            if (size > 0) {
-//                data.setmBuffer(lin);
-//                data.setmSize(size);
-//                list.add(data);
-//            }
-//
-//        }
-//        return list;
-//    }
-
-    public static Object[] splitAry(byte[] ary, int subSize) {
-        int count = ary.length % subSize == 0 ? ary.length / subSize: ary.length / subSize + 1;
-
-        List<List<Byte>> subAryList = new ArrayList<List<Byte>>();
-
-        for (int i = 0; i < count; i++) {
-            int index = i * subSize;
-            List<Byte> list = new ArrayList<Byte>();
-            int j = 0;
-            while (j < subSize && index < ary.length) {
-                list.add(ary[index++]);
-                j++;
-            }
-            subAryList.add(list);
-        }
-
-        Object[] subAry = new Object[subAryList.size()];
-
-        for(int i = 0; i < subAryList.size(); i++){
-            List<Byte> subList = subAryList.get(i);
-            byte[] subAryItem = new byte[subList.size()];
-            for(int j = 0; j < subList.size(); j++){
-                subAryItem[j] = subList.get(j);
-            }
-            subAry[i] = subAryItem;
-        }
-
-        return subAry;
     }
 
 
@@ -239,13 +115,5 @@ public abstract class MultiAudioMixer {
             return realMixAudio;
         }
 
-    }
-
-    public Speex getSpeex() {
-        return speex;
-    }
-
-    public void setSpeex(Speex speex) {
-        this.speex = speex;
     }
 }
